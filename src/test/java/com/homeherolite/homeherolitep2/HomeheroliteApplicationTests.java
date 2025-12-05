55057910eec28d24797ee9dc19d197aff5067497
pedroguaita <pedropguaita@gmail.com>
2025-12-05 17:31:51 -0300

    Projeto criado

---
+++ src/test/java/com/homeherolite/homeherolitep2/HomeheroliteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.homeherolite.homeherolitep2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeheroliteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
